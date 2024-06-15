075368f36c28ee66d4c7e9a1880561627f789a4c
joeyturbo <joeyturbo@gmail.com>
2024-06-15 08:16:00 -0400

    created ui buttons and started adding event listeners

--- script.js
+++ script.js
@@ -46,40 +46,46 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-function playGame() {
-  let rounds = 1;
+// function playGame() {
+//   let rounds = 1;
 
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === 'rock' && computerChoice === 'scissors' ||
-        humanChoice === 'rock' && computerChoice === 'paper' ||
-        humanChoice === 'scissors' && computerChoice === 'paper' ||
-        humanChoice === 'paper' && computerChoice === 'rock'
-    ) {
-      humanScore++;
-      console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
-    } else if (
-        humanChoice === computerChoice 
-    ) {
-      console.log(`It's a draw! You both chose ${humanChoice}`);
-    } else {
-      computerScore++;
-      console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
-    }
-    rounds++;
-}
+//   function playRound(humanChoice, computerChoice) {
+//     if (humanChoice === 'rock' && computerChoice === 'scissors' ||
+//         humanChoice === 'rock' && computerChoice === 'paper' ||
+//         humanChoice === 'scissors' && computerChoice === 'paper' ||
+//         humanChoice === 'paper' && computerChoice === 'rock'
+//     ) {
+//       humanScore++;
+//       console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+//     } else if (
+//         humanChoice === computerChoice 
+//     ) {
+//       console.log(`It's a draw! You both chose ${humanChoice}`);
+//     } else {
+//       computerScore++;
+//       console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+//     }
+//     rounds++;
+// }
 
-  while (rounds <= 5) {
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-  if (humanScore > computerScore) {
-    console.log(`You Won! Score --- Human: ${humanScore}, Computer: ${computerScore}`)
-  } else if (humanScore === computerScore) {
-    console.log(`It's a Draw! Score --- Human: ${humanScore}, Computer: ${computerScore}`)
-  } else {
-    console.log(`You Lose! Score --- Human: ${humanScore}, Computer: ${computerScore}`)
-  }
-}
+//   while (rounds <= 5) {
+//     playRound(getHumanChoice(), getComputerChoice());
+//   }
+//   if (humanScore > computerScore) {
+//     console.log(`You Won! Score --- Human: ${humanScore}, Computer: ${computerScore}`)
+//   } else if (humanScore === computerScore) {
+//     console.log(`It's a Draw! Score --- Human: ${humanScore}, Computer: ${computerScore}`)
+//   } else {
+//     console.log(`You Lose! Score --- Human: ${humanScore}, Computer: ${computerScore}`)
+//   }
+// }
 
-// console.log(getComputerChoice());
+// // console.log(getComputerChoice());
 
-playGame();
+// playGame();
+
+const btnList = document.querySelectorAll('.btn');
+
+btnList.forEach((btn) => {
+  btn.addEventListener('click', playRound(btn.id, getComputerChoice) )
+})
